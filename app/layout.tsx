3998c1f1ffef8532ed96742e0ef6aaf6028cb859
sitrakiniainadiary15-sketch <sitrakiniainadiary15@gmail.com>
2026-07-30 11:09:40 +0300

    Ajout analytics - non teste

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -35,6 +36,7 @@ export default function RootLayout({
           </BookCallProvider>
         </LanguageProvider>
       </body>
+      <GoogleAnalytics gaId="G-57F7Y4KS48" />
     </html>
   );
 }
